4153472a5e0957361317dfc22d250864f7909fbd
sonohen <sonohen55@gmail.com>
2024-06-22 20:04:25 +0900

    Initial commit

---
+++ test/sample_secured-ec2_in_private-subnet.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SampleSecuredEc2InPrivateSubnet from '../lib/sample_secured-ec2_in_private-subnet-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/sample_secured-ec2_in_private-subnet-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SampleSecuredEc2InPrivateSubnet.SampleSecuredEc2InPrivateSubnetStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
